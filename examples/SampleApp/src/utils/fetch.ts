eb68e9919d1859ba3c815c3d2466637ac6fef556
akul <akulgupta2000@gmail.com>
2022-08-21 19:24:05 -0700

    don't re-fetch if identical request inflight

--- examples/SampleApp/src/utils/fetch.ts
+++ examples/SampleApp/src/utils/fetch.ts
@@ -1,12 +1,25 @@
 //TODO: If you send a debug release app to testflight this will cause the whole app to fail...
+// import hash from 'object-hash';
+// import AsyncStore from './AsyncStore';
+
 const origin = __DEV__
-  ? 'http://192.168.30.103:5001/game-7bb7c/us-central1'
+  ? 'http://192.168.30.101:5001/game-7bb7c/us-central1'
   : 'https://us-central1-game-7bb7c.cloudfunctions.net';
 
-export const fetchPost = (endpoint: string, data: Record<any, any>) => {
+const cache = new Set();
+
+export const fetchPost = (endpoint: string, data: Record<any, any>, ignoreDupIfPending = true) => {
+  let hash = '';
+  if (ignoreDupIfPending) {
+    hash = JSON.stringify(data);
+    if (cache.has(hash)) {
+      return Promise.resolve({});
+    }
+    cache.add(hash);
+  }
+
   const url = `${origin}/${endpoint}`;
   console.log(url);
-  console.log(`[fetchPost] fetting url ${url}`);
   return new Promise((resolve, reject) => {
     fetch(url, {
       body: JSON.stringify(data),
@@ -15,6 +28,9 @@ export const fetchPost = (endpoint: string, data: Record<any, any>) => {
       },
       method: 'POST',
     }).then(async (response) => {
+      if (ignoreDupIfPending) {
+        cache.delete(hash);
+      }
       if (response.status !== 200 || !response.ok) {
         reject('bad response');
       }
